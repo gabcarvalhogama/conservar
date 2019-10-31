7eefa7c28b0348fc5c8095c51293a9e2b3fcc177
Gabriel Carvalho Gama <gabriel.carvalhogama@hotmail.com>
2019-10-31 01:07:07 -0300

    Added copyrights footer

--- footer.php
+++ footer.php
@@ -7,6 +7,10 @@
                         <li><a href=""><i class="fab fa-facebook"></i></a></li>
                     </ul>
                 </div>
+                <div class="footer--rights">
+                    <p>Todos os direitos reservados.</p>
+                    <a href="https://maxdd.com.br" target="_blank" rel="nofollow">Desenvolvido com excelência pela Max Design Digital.</a>
+                </div>
             </div>
         </footer>
         
--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 	   	
 	   	<!-- Link Tag -->
 	   	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-		<link rel="icon" type="image/png" href="<?=get_site_icon_url(512, bloginfo("template_url").'/images/favicon.png')?>" />
+		<link rel="icon" type="image/png" href="<?=get_site_icon_url(512)?>" />
 		<link rel="stylesheet" href="<?=bloginfo("template_url")?>/css/styles.css" type="text/css" />
     </head>
     <body>
